a276c568f0e2bfb2d84f390af678951f35868bbf
Haridas98 <Haridas98@mail.ru>
2026-04-05 12:34:43 +0500

    fix: remove SessionAuthentication to fix 403 CSRF error on registration

--- backend/jewelry_store/settings.py
+++ backend/jewelry_store/settings.py
@@ -120,10 +120,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 50,  # Увеличили с 20 до 50
+    'PAGE_SIZE': 50,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
